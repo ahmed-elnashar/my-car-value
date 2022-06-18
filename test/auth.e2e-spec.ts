f34bada2c405e47fa9835d67ee3ed8f911b9ed1e
Ahmed El Nashar <a7medelnashar@hotmail.com>
2022-06-18 17:18:12 +0200

    configured nestConfig and prepared the env for e2e testing

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System (e2e)', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it('handles a signup request', () => {
+        const EMAIL = 'ashl3e@fsd.com';
+        return request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({
+                email: EMAIL,
+                password: 'fsalfdkl'
+            })
+            .expect(201)
+            .then((res) => {
+                const { id, email } = res.body;
+                expect(id).toBeDefined();
+                expect(email).toEqual(EMAIL);
+            });
+    });
+
+});
