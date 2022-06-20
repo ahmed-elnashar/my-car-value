d7d1b7807d06d7f23c161cb78dbdf9986441a4db
Ahmed El Nashar <a7medelnashar@hotmail.com>
2022-06-20 23:27:42 +0200

    added relationship between users and reports

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-reports.dto';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+    constructor(
+        @InjectRepository(Report) private repo: Repository<Report>
+    ) { }
+
+    create(reportDto: CreateReportDto, user: User) {
+        const report = this.repo.create(reportDto);
+        report.user = user;
+        return this.repo.save(report);
+    }
+}
