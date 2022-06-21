d2d892d4787f1008bd40df0829a82cec657d95d5
Ahmed El Nashar <a7medelnashar@hotmail.com>
2022-06-21 17:44:46 +0200

    added cookie-key as an env-variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,12 +45,15 @@ const cookieSession = require('cookie-session');
 
 export class AppModule {
 
-  constructor(private dataSource: DataSource) { }
+  constructor(
+    private dataSource: DataSource,
+    private configService: ConfigService
+  ) { }
 
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(cookieSession({
-        keys: ['asdfasdfsdaf']
+        keys: [this.configService.get('COOKIE_KEY')]
       }))
       .forRoutes('*');
   }
